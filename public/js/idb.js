797ca27dcae3936e69241eaaa7d79b8bb0889fcc
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-01-25 23:07:19 -0500

    added idb.js to index.html and added to idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,62 @@ request.onsuccess = function(event) {
 request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
+
+// function if there is no internet connection and attempt to submit a new transaction
+function saveRecord(record) {
+
+    // opens a new transaction with db with permission to read and write
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access object store
+    const budgetObjectScore = transaction.objectScore('new_transaction');
+
+    // add record to your store with add method
+    budgetObjectScore.add(record);
+};
+
+function uploadTransaction() {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectScore = transaction.objectScore('new_transaction');
+
+    // get records from store and set to variable
+    const getAll = budgetObjectScore.getAll();
+
+    // upon succcess .get() function
+    getAll.onsuccess = function () {
+        if (getAll.result.legth > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    //open another transaction
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    
+                    // access the new_transaction object store
+                    const budgetObjectScore = transaction.objectScore('new_transaction');
+
+                    // clear all items in store
+                   budgetObjectScore.clear();
+
+                   alert('All saved transactions have been saved.');
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
